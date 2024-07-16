b857ecd58273a560450090f2f81d45ee52247970
Moises Vega <moises.vega@gmail.com>
2024-07-15 19:44:44 -0700

    Use cobra cli

--- main.go
+++ main.go
@@ -1,21 +1,30 @@
 package main
 
 import (
-	"flag"
 	"log"
 	"os"
 
 	"github.com/moisesvega/diffy/internal/client"
+	"github.com/moisesvega/diffy/internal/config"
+	"github.com/spf13/cobra"
 )
 
 func main() {
 	log.SetFlags(0) // Removes timestamp
 
 	// TODO: Make it configurable
-	r := runner{
-		flagSet: flag.NewFlagSet("diffy", flag.ContinueOnError),
+	r := runner{cfg: &config.Config{}}
+	cmd := cobra.Command{
+		Use:           "diffy",
+		Short:         "CLI designed to deliver comprehensive statistics and insights from code reviews and differential analysis",
+		SilenceUsage:  true,
+		SilenceErrors: true,
+		RunE: func(cmd *cobra.Command, args []string) error {
+			return r.run(cmd.Flags().Args())
+		},
 	}
-	if err := r.run(os.Args[1:]); err != nil {
+	registerFlags(cmd.Flags(), r.cfg)
+	if err := cmd.Execute(); err != nil {
 		log.Fatal(err)
 	}
 }
