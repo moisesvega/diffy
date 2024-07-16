daad5b0b068e55a28da6b5f22dca497df4b07ab4
Moises Vega <moises.vega@gmail.com>
2024-07-15 19:46:13 -0700

    Update runner with new flagsets

--- runner.go
+++ runner.go
@@ -1,17 +1,28 @@
 package main
 
 import (
-	"flag"
+	"log"
+	"strings"
 
 	"github.com/moisesvega/diffy/internal/client"
+	"github.com/moisesvega/diffy/internal/config"
 )
 
 type runner struct {
-	flagSet    *flag.FlagSet
+	cfg        *config.Config
 	phabClient *client.Client
 }
 
 func (r *runner) run(args []string) error {
-	r.flagSet.Parse(args)
+	if r.cfg.Settings {
+		return editSettings()
+	}
+	log.Printf("Running %s", strings.Join(args, " "))
+	log.Println(r.cfg)
+	return nil
+}
+
+func editSettings() error {
+	// TODO: Open settings.yaml and a allow user to edit it.
 	return nil
 }
