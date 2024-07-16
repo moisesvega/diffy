d2cffb7da720f1109803810ca9cfa3331ade206e
Moises Vega <moises.vega@gmail.com>
2024-07-16 10:38:51 -0700

    Update configuration with Phabricaton Configuration

--- flags.go
+++ flags.go
@@ -9,5 +9,12 @@ func registerFlags(fs *pflag.FlagSet, cfg *config.Config) *pflag.FlagSet {
 	fs.BoolVar(&cfg.Settings, "settings", false, "edit settings")
 	fs.StringSliceVar(&cfg.PhabUsers, "phab_users", nil, "phab_users=user_1,user_2")
 	fs.StringSliceVar(&cfg.GithubUsers, "github_users", nil, "github_users=user_1,user_2")
+
+	// Phabricator Configuration
+	fs.StringVar(&cfg.PhabricatorConfig.APIToken, "api_token", "", "api_token=token")
+	fs.StringVar(&cfg.PhabricatorConfig.AccessToken, "access_token", "", "access_token=token")
+	fs.StringVar(&cfg.PhabricatorConfig.ArcrcFilePath, "arrc_file", "~/.arcrc", "arrc_file=~/.arcrc")
+
+	// Github Configuration
 	return fs
 }
--- internal/config/config.go
+++ internal/config/config.go
@@ -1,7 +1,14 @@
 package config
 
 type Config struct {
-	Settings    bool
-	PhabUsers   []string
-	GithubUsers []string
+	Settings          bool
+	PhabUsers         []string
+	GithubUsers       []string
+	PhabricatorConfig PhabricatorConfig
+}
+
+type PhabricatorConfig struct {
+	ArcrcFilePath string `yaml:"arrc_file_path"`
+	APIToken      string `yaml:"api_token"`
+	AccessToken   string `yaml:"access_token"`
 }
