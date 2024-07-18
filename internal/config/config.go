961b67bb611cc74a358e8a440e405aa14d788e75
Moises Vega <moises.vega@gmail.com>
2024-07-17 23:19:10 -0700

    Add test for configuration

--- internal/config/config.go
+++ internal/config/config.go
@@ -7,29 +7,45 @@ import (
 	"gopkg.in/yaml.v3"
 )
 
+// Config root configuration structure
 type Config struct {
-	Settings          bool
-	PhabUsers         []string          `yaml:"phab_users"`
-	GithubUsers       []string          `yaml:"github_users"`
+	Settings    bool
+	PhabUsers   []string `yaml:"phab_users"`
+	GithubUsers []string `yaml:"github_users"`
+
 	PhabricatorConfig PhabricatorConfig `yaml:"phabricator_config"`
 }
 
+// PhabricatorConfig configuration to set a proper Phabricator Client
 type PhabricatorConfig struct {
-	URL           string `yaml:"url"`
-	APIToken      string `yaml:"api_token"`
-	AccessToken   string `yaml:"access_token"`
-	ArcrcFilePath string `yaml:"arrc_file_path"`
+	URL            string `yaml:"url"`
+	APIToken       string `yaml:"api_token"`
+	APITokenEnv    string `yaml:"api_token_env"`
+	AccessToken    string `yaml:"access_token"`
+	AccessTokenEnv string `yaml:"access_token_env"`
+	ArcrcFilePath  string `yaml:"arrc_file_path"`
 }
 
+// ReadConfiguration from a given filepath
 func ReadConfiguration(filepath string) (*Config, error) {
 	yamlFile, err := os.ReadFile(filepath)
 	if err != nil {
 		return nil, fmt.Errorf("failed to read configuration file %s: %w", filepath, err)
 	}
-	fmt.Println(string(yamlFile))
 	cfg := &Config{}
 	if err := yaml.Unmarshal(yamlFile, cfg); err != nil {
 		return nil, fmt.Errorf("unable to parse config.yaml: %w", err)
 	}
 	return cfg, nil
+}
+
+// DefaultConfiguration returns default configuration.
+func DefaultConfiguration() *Config {
+	return &Config{
+		PhabricatorConfig: PhabricatorConfig{
+			APITokenEnv:    "PHAB_API_TOKEN",
+			AccessTokenEnv: "PHAB_ACCESS_TOKEN",
+			ArcrcFilePath:  "~/.arcrc",
+		},
+	}
 }
--- internal/config/config_test.go
+++ internal/config/config_test.go
@@ -1,13 +1,59 @@
 package config
 
 import (
+	"os"
+	"path"
 	"testing"
 
 	"github.com/stretchr/testify/require"
+	"gopkg.in/yaml.v3"
 )
 
 func TestReadConfiguration(t *testing.T) {
-	cfg, err := ReadConfiguration("testdata/config.yaml")
+	file := `
+phab_users:
+  - user
+  - user1
+github_users:
+  - user
+  - user1
+phabricator_config:
+  url: "someURL"
+`
+	fp := path.Join(t.TempDir(), "test.yaml")
+	require.NoError(t, os.WriteFile(fp, []byte(file), 0644))
+	cfg, err := ReadConfiguration(fp)
 	require.NoError(t, err)
-	require.NotNil(t, cfg)
+	require.NotEmpty(t, cfg)
+}
+
+func TestReadConfigurationParseError(t *testing.T) {
+	file := `
+phab_users:
+  - phabricator_config:
+`
+	fp := path.Join(t.TempDir(), "test.yaml")
+	require.NoError(t, os.WriteFile(fp, []byte(file), 0644))
+	cfg, err := ReadConfiguration(fp)
+	require.Error(t, err)
+	require.Nil(t, cfg)
+}
+
+func TestReadConfigurationFileNotFound(t *testing.T) {
+	fp := path.Join(t.TempDir(), "test.yaml")
+	cfg, err := ReadConfiguration(fp)
+	require.Error(t, err)
+	require.Nil(t, cfg)
+}
+
+func TestWriteConfiguration(t *testing.T) {
+	defaults := DefaultConfiguration()
+	got, err := yaml.Marshal(defaults)
+	require.NoError(t, err)
+	require.NotEmpty(t, got)
+	// os.WriteFile(path.Join("./testdata/defaults.yaml"), got, 0644)
+	want, err := os.ReadFile("./testdata/defaults.yaml")
+	require.NoError(t, err)
+	require.NotEmpty(t, want)
+	require.Equal(t, string(got), string(want))
 }
