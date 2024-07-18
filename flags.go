5cc381d6524847f9944fccea2b6a53cc6ebbd2d9
Moises Vega <moises.vega@gmail.com>
2024-07-17 22:12:57 -0700

    Add flag descriptions

--- flags.go
+++ flags.go
@@ -8,17 +8,18 @@ import (
 func registerFlags(fs *pflag.FlagSet, cfg *config.Config) *pflag.FlagSet {
 	// TODO: Create proper descriptions
 	fs.BoolVar(&cfg.Settings, "settings", false, "edit settings")
-	fs.StringSliceVar(&cfg.PhabUsers, "phab_users", nil, "phab_users=user_1,user_2")
-	fs.StringSliceVar(&cfg.GithubUsers, "github_users", nil, "github_users=user_1,user_2")
+	fs.StringSliceVar(&cfg.PhabUsers, "phab_users", nil, "List of phabricator users you want to track.")
+	fs.StringSliceVar(&cfg.GithubUsers, "github_users", nil, "List of github users you want to track.")
 
 	// Phabricator Configuration
 	// TODO: Look into how to parse the URL in this step
-	fs.StringVar(&cfg.PhabricatorConfig.URL, "phab_url", "", "api_token=token")
-	fs.StringVar(&cfg.PhabricatorConfig.APIToken, "phab_api_token", "", "api_token=token")
-	fs.StringVar(&cfg.PhabricatorConfig.AccessToken, "phab_access_token", "", "access_token=token")
-	fs.StringVar(&cfg.PhabricatorConfig.ArcrcFilePath, "arrc_file", "", "arrc_file=~/.arcrc")
+	fs.StringVar(&cfg.PhabricatorConfig.URL, "phab_url", "", "Phabricator API url")
+	fs.StringVar(&cfg.PhabricatorConfig.APIToken, "phab_api_token", "", "API token this could be inside of your .arrc")
+	fs.StringVar(&cfg.PhabricatorConfig.AccessToken, "phab_access_token", "", "If API URL is protected by oauth you can provide your access token here")
+	fs.StringVar(&cfg.PhabricatorConfig.ArcrcFilePath, "arrc_file", "", "If provided it will read the .arcrc and get URL and API Token")
 
+	// Github Configurati
+	// TODO: Create github configuration
 	fs.SortFlags = false
-	// Github Configuration
 	return fs
 }
